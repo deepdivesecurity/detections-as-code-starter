95933694062bac7771e0ba5a0eb3a6aa7bfbd20e
deepdivesecurity <165413290+deepdivesecurity@users.noreply.github.com>
2026-07-10 22:47:41 -0400

    Removing old files

--- mappings/sentinel.py
+++
@@ -1,9 +0,0 @@
-from sigma.processing.pipeline import ProcessingPipeline
-
-def sentinel_pipeline():
-    return ProcessingPipeline(
-        name="sentinel-custom",
-        category_to_table_mapping={
-            "okta": "Okta_CL"
-        },
-    )
